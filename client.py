e4e77fdd503001d8f9717ef0fca68160a3a3e9b8
gitmobkab <richmondaurel77@gmail.com>
2025-08-17 19:10:26 +0000

    refactor client and server code for clarity and consistency

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+SERVER_HOST = "127.0.0.1"  # The server's hostname or IP address
+SERVER_PORT = 65432  # The port used by the server
+NICKNAME = "Mob (@admin)"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((SERVER_HOST, SERVER_PORT))
+    msg = bytes(NICKNAME,encoding="utf-8")
+    client.sendall(msg)
+    data = client.recv(1024)
 
-print(f"Received {data!r}")
+print(data)
--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+# HOST = socket.gethostbyname(socket.gethostname())
+HOST = "127.0.0.1" # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+DEFAULT_BYTE_SIZE = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen()
+    client, addr = server.accept()
+    with client:
         print(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024)
+            data = client.recv(DEFAULT_BYTE_SIZE)
             if not data:
                 break
-            conn.sendall(data)
+            
+            client.sendall(data+b" As joined the chat")
