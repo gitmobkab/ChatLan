3e5e9310fee1ff2591479b0a6883836c07ce52f8
gitmobkab <richmondaurel77@gmail.com>
2025-10-28 23:15:21 +0000

    implemented Alert widget styling

--- chat_widget.py
+++ chat_widget.py
@@ -11,6 +11,7 @@ class Message(Static):
     margin: 2 0;
     border: panel $foreground;
     padding: 2;
+    width: 75vw;
 }"""
 
     def __init__(self, *children: Static, name: str | None = None, id: str | None = None, classes: str | None = None, disabled: bool = False, markup: bool = True,title: str = "Title", content: str = "", mode: Literal["info","warning","alert"] | None = None) -> None:
@@ -29,6 +30,33 @@ class Message(Static):
         markdown_text = Markdown(self.content)
         return markdown_text
     
+
+class Alert(Static):
+
+    DEFAULT_CSS="""Alert{
+    height: auto;
+    margin: 2 40;
+    border: outer $foreground;
+    padding: 2;
+    width: 50vw;
+    text-align: center;
+    content-align: center middle;
+}"""
+
+    def __init__(self, content = "", *, expand = False, shrink = False, markup = True, name = None, id = None, classes = None, disabled = False,
+                 type: Literal["new_connection","disconnection"] | None = None):
+        super().__init__(content, expand=expand, shrink=shrink, markup=markup, name=name, id=id, classes=classes, disabled=disabled)
+        self.type = type
+        
+    def on_mount(self): 
+        self.border_subtitle = strftime("[ %I:%M ]")
+    
+    def render(self) -> RenderResult:
+        if self.type:
+            self.add_class(self.type)
+        markdown_text = Markdown(self.content)
+        return markdown_text
+    
 class MyMessageApp(App):
     CSS_PATH="chatlan.tcss"
     
@@ -38,6 +66,10 @@ class MyMessageApp(App):
         yield Message(content="Message, `world`" ,mode="info")
         yield Message(content="Message, `world`" ,mode="warning")
         yield Message(content="Message, `world`" ,mode="alert")
+        yield Alert(content="Hello *friend*")
+        yield Alert(content="Heelo *Boys*", type="new_connection")
+        yield Alert(content="HELLO `GIRLS`", type="disconnection")
+        
         
     
 if __name__=="__main__":
