8ee4bd8d8f386db8bc375cae0cee23f48afee3fc
gitmobkab <richmondaurel77@gmail.com>
2025-12-16 20:03:12 +0000

    implement format_msg function

--- src/chatlan/utils.py
+++ src/chatlan/utils.py
@@ -12,3 +12,25 @@ def echo(message:str,mode: Literal["info","success","warning","error"]):
     prefix = prefixs[mode]
     rprint(f"{prefix} [bold]{message}")
     
+def format_msg(message: str | bytes) -> bytes:
+    """
+    Take a message either in a string or a bytes
+    
+    always add the `\\n` character to the end of the message
+    
+    and return it as a bytes
+    
+    ie:
+        - format_msg("Mob Joined the Chat") -> b"Mob Joined The Chat \\n"
+        - format_msg(b"Mob Joined the Chat") -> b"Mob Joined The Chat \\n"
+        - format_msg("Mob \\nJoined the Chat") -> b"Mob \\nJoined The Chat \\n"
+    """
+    if type(message) is str:
+        formatted_msg = message + "\n"
+        formatted_msg = formatted_msg.encode()
+        return formatted_msg
+    
+    if type(message) is bytes:
+        return message + b"\n"
+    
+    return b""
